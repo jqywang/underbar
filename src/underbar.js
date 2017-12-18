022ec9c9fd394826c73980562ee22928af6852e0
Jeffrey Wang <jqywang.95@gmail.com>
2017-12-18 12:15:51 -0800

    "_.extend now passes all tests"

--- src/underbar.js
+++ src/underbar.js
@@ -251,6 +251,14 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    if(arguments.length > 1){
+      for(var i = 1; i<arguments.length; i++){
+        for(var element in arguments[i]){
+          obj[element]= arguments[i][element];
+        }
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
